08759c6020dbc9aae062da27c4548ad5b2db7f0c
umesh-9 <umeshnaidu00@gmail.com>
2023-07-31 19:41:27 +0530

    created GetStarted Round logo component

---
+++ modern_bank_app/src/components/GetStarted.jsx
@@ -0,0 +1,30 @@
+import { arrowUp } from "../assets";
+import styles from "../style";
+
+const GetStarted = () => {
+  return (
+    <div
+      className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer `}
+    >
+      <div
+        className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full hover:bg-cyan-700 `}
+      >
+        <div className={`${styles.flexStart} flex-row `}>
+          <p className="font-poppins font-medium text-[18px]  mr-2 leading-[23px">
+            <span className="text-gradient">Get</span>
+          </p>
+          <img
+            src={arrowUp}
+            className="w-[23px] h-[23px] object-contain"
+            alt="arrow"
+          />
+        </div>
+        <p className="font-poppins font-medium text-[18px]  leading-[23px">
+          <span className="text-gradient">Started</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default GetStarted;
